e468b533c7093b8b718b896a6a68321055fa414e
MAdriaansens <132375271+MAdriaansens@users.noreply.github.com>
2026-01-23 16:18:51 +1300

    Create Run_sratoolkit.sh

---
+++ download_sra/Run_sratoolkit.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+#SBATCH --job-name=BacSCAN
+#SBATCH --time=72:00:00      # Walltime (HH:MM:SS)
+#SBATCH --mem=50GB          # Memory in MB
+#SBATCH --cpus-per-task=10
+#SBATCH --array=0-6
+#SBATCH --account=uc04105
+#SBATCH --output=slurm_sra/sra_output%A-%a.out
+#SBATCH --error=slurm_sra/sra_error%A-%a.err
+declare -a array=(1 2 3 4 5 6)
+module load sratoolkit/3.0.2
+ACCESSION_LIST=/nesi/nobackup/uc04105/cross_biome_metagenome/raw_data/water/SAR/SRR_table${array[$SLURM_ARRAY_TASK_ID]}.txt
+
+
+# Check if the accessions file exists
+if [ ! -f "$ACCESSION_LIST" ]; then
+    echo "Error: Accession list file not found!"
+    exit 1
+fi
+
+# Loop through the accessions and run fasterq-dump
+while read SRR_ID; do
+    echo "Starting download and conversion for $SRR_ID"
+    fasterq-dump $SRR_ID --threads 8
+    echo "Finished $SRR_ID"
+done < "$ACCESSION_LIST"
