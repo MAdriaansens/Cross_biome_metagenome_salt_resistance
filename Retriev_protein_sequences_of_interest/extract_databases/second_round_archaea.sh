9164d75dfd934a6ef8ce8f5f83184a50e8d5000a
MAdriaansens <132375271+MAdriaansens@users.noreply.github.com>
2026-02-24 09:18:32 +1300

    Update second_round_archaea.sh

--- Retriev_protein_sequences_of_interest/extract_databases/second_round_archaea.sh
+++ Retriev_protein_sequences_of_interest/extract_databases/second_round_archaea.sh
@@ -19,7 +19,7 @@ mmseqs easy-search -e 1.00E-03 -c 0.0 --threads 10 ${Seq}/PF00318_RpS2_merged.fa
 
 module load Python/3.11.3-gimkl-2022a
 
-python getting_fasta_from_hit_extra_Euk.py ${MMseqs}/PF00318_RpS2merged_vsArchaea${array[$SLURM_ARRAY_TASK_ID]}_e03_mmseq.tsv MMSEQ ${Arc_TSV} ${MMseqs}/PF00318_RpS2merged_vsArchaea${array[$SLURM_ARRAY_TASK_ID]}_e03mmseq_fl_seq.fasta
+python getting_fasta_from_hit_extra_Arc.py ${MMseqs}/PF00318_RpS2merged_vsArchaea${array[$SLURM_ARRAY_TASK_ID]}_e03_mmseq.tsv MMSEQ ${Arc_TSV} ${MMseqs}/PF00318_RpS2merged_vsArchaea${array[$SLURM_ARRAY_TASK_ID]}_e03mmseq_fl_seq.fasta
 
 #----------------------------------------------------------RpL4-------------------------------------------------------------------------
 module load MMseqs2/15-6f452-gompi-2023a
@@ -27,7 +27,7 @@ mmseqs easy-search -e 1.00E-03 -c 0.0 --threads 10 ${Seq}/PF00573_RpL4_merged.fa
 
 module load Python/3.11.3-gimkl-2022a
 
-python getting_fasta_from_hit_extra_Euk.py ${MMseqs}/PF00573_RpL4merged_vsArchaea${array[$SLURM_ARRAY_TASK_ID]}_e03_mmseq.tsv MMSEQ ${Arc_TSV} ${MMseqs}/PF00573_RpL4merged_vsArchaea${array[$SLURM_ARRAY_TASK_ID]}_e03mmseq_fl_seq.fasta
+python getting_fasta_from_hit_extra_Arc.py ${MMseqs}/PF00573_RpL4merged_vsArchaea${array[$SLURM_ARRAY_TASK_ID]}_e03_mmseq.tsv MMSEQ ${Arc_TSV} ${MMseqs}/PF00573_RpL4merged_vsArchaea${array[$SLURM_ARRAY_TASK_ID]}_e03mmseq_fl_seq.fasta
 
 #----------------------------------------------------------MIP-------------------------------------------------------------------------
 module load MMseqs2/15-6f452-gompi-2023a
@@ -35,7 +35,7 @@ mmseqs easy-search -e 1.00E-03 -c 0.0 --threads 10 ${Seq}/PF00230_MIP_merged.fas
 
 module load Python/3.11.3-gimkl-2022a
 
-python getting_fasta_from_hit_extra_Euk.py ${MMseqs}/PF00230_MIPmerged_vsArchaea${array[$SLURM_ARRAY_TASK_ID]}_e03_mmseq.tsv MMSEQ ${Arc_TSV} ${MMseqs}/PF00230_MIPmerged_vsArchaea${array[$SLURM_ARRAY_TASK_ID]}_e03mmseq_fl_seq.fasta
+python getting_fasta_from_hit_extra_Arc.py ${MMseqs}/PF00230_MIPmerged_vsArchaea${array[$SLURM_ARRAY_TASK_ID]}_e03_mmseq.tsv MMSEQ ${Arc_TSV} ${MMseqs}/PF00230_MIPmerged_vsArchaea${array[$SLURM_ARRAY_TASK_ID]}_e03mmseq_fl_seq.fasta
 
 #----------------------------------------------------------ClCA-------------------------------------------------------------------------
 module load MMseqs2/15-6f452-gompi-2023a
@@ -43,7 +43,7 @@ mmseqs easy-search -e 1.00E-03 -c 0.0 --threads 10 ${Seq}/PF00654_ClcA_merged.fa
 
 module load Python/3.11.3-gimkl-2022a
 
-python getting_fasta_from_hit_extra_Euk.py ${MMseqs}/PF00654_ClcAmerged_vsArchaea${array[$SLURM_ARRAY_TASK_ID]}_e03_mmseq.tsv MMSEQ ${Arc_TSV} ${MMseqs}/PF00654_ClcAmerged_vsArchaea${array[$SLURM_ARRAY_TASK_ID]}_e03mmseq_fl_seq.fasta
+python getting_fasta_from_hit_extra_Arc.py ${MMseqs}/PF00654_ClcAmerged_vsArchaea${array[$SLURM_ARRAY_TASK_ID]}_e03_mmseq.tsv MMSEQ ${Arc_TSV} ${MMseqs}/PF00654_ClcAmerged_vsArchaea${array[$SLURM_ARRAY_TASK_ID]}_e03mmseq_fl_seq.fasta
 
 #---------------------------------------------------------MscS-------------------------------------------------------------------------
 module load MMseqs2/15-6f452-gompi-2023a
@@ -51,7 +51,7 @@ mmseqs easy-search -e 1.00E-03 -c 0.0 --threads 10 ${Seq}/PF00924_MscS_merged.fa
 
 module load Python/3.11.3-gimkl-2022a
 
-python getting_fasta_from_hit_extra_Euk.py ${MMseqs}/PF00924_MscSmerged_vsArchaea${array[$SLURM_ARRAY_TASK_ID]}_e03_mmseq.tsv MMSEQ ${Arc_TSV} ${MMseqs}/PF00924_MscSmerged_vsArchaea${array[$SLURM_ARRAY_TASK_ID]}_e03mmseq_fl_seq.fasta
+python getting_fasta_from_hit_extra_Arc.py ${MMseqs}/PF00924_MscSmerged_vsArchaea${array[$SLURM_ARRAY_TASK_ID]}_e03_mmseq.tsv MMSEQ ${Arc_TSV} ${MMseqs}/PF00924_MscSmerged_vsArchaea${array[$SLURM_ARRAY_TASK_ID]}_e03mmseq_fl_seq.fasta
 
 #---------------------------------------------------------Na_alanine_symporter-------------------------------------------------------------------------
 module load MMseqs2/15-6f452-gompi-2023a
@@ -59,7 +59,7 @@ mmseqs easy-search -e 1.00E-03 -c 0.0 --threads 10 ${Seq}/PF01235_Na_alanine_sym
 
 module load Python/3.11.3-gimkl-2022a
 
-python getting_fasta_from_hit_extra_Euk.py ${MMseqs}/PF01235_Na_alanine_symportermerged_vsArchaea${array[$SLURM_ARRAY_TASK_ID]}_e03_mmseq.tsv MMSEQ ${Arc_TSV} ${MMseqs}/PF01235_Na_alanine_symportermerged_vsArchaea${array[$SLURM_ARRAY_TASK_ID]}_e03mmseq_fl_seq.fasta
+python getting_fasta_from_hit_extra_Arc.py ${MMseqs}/PF01235_Na_alanine_symportermerged_vsArchaea${array[$SLURM_ARRAY_TASK_ID]}_e03_mmseq.tsv MMSEQ ${Arc_TSV} ${MMseqs}/PF01235_Na_alanine_symportermerged_vsArchaea${array[$SLURM_ARRAY_TASK_ID]}_e03mmseq_fl_seq.fasta
 
 #---------------------------------------------------------Na_alanine_symporter-------------------------------------------------------------------------
 module load MMseqs2/15-6f452-gompi-2023a
@@ -67,7 +67,7 @@ mmseqs easy-search -e 1.00E-03 -c 0.0 --threads 10 ${Seq}/PF01544_CorA_merged.fa
 
 module load Python/3.11.3-gimkl-2022a
 
-python getting_fasta_from_hit_extra_Euk.py ${MMseqs}/PF01544_CorAmerged_vsArchaea${array[$SLURM_ARRAY_TASK_ID]}_e03_mmseq.tsv MMSEQ ${Arc_TSV} ${MMseqs}/PF01544_CorAmerged_vsArchaea${array[$SLURM_ARRAY_TASK_ID]}_e03mmseq_fl_seq.fasta
+python getting_fasta_from_hit_extra_Arc.py ${MMseqs}/PF01544_CorAmerged_vsArchaea${array[$SLURM_ARRAY_TASK_ID]}_e03_mmseq.tsv MMSEQ ${Arc_TSV} ${MMseqs}/PF01544_CorAmerged_vsArchaea${array[$SLURM_ARRAY_TASK_ID]}_e03mmseq_fl_seq.fasta
 
 #---------------------------------------------------------Ca_exchanger-------------------------------------------------------------------------
 module load MMseqs2/15-6f452-gompi-2023a
@@ -75,7 +75,7 @@ mmseqs easy-search -e 1.00E-03 -c 0.0 --threads 10 ${Seq}/PF01699_Ca_exchanger_m
 
 module load Python/3.11.3-gimkl-2022a
 
-python getting_fasta_from_hit_extra_Euk.py ${MMseqs}/PF01699_Ca_exchangermerged_vsArchaea${array[$SLURM_ARRAY_TASK_ID]}_e03_mmseq.tsv MMSEQ ${Arc_TSV} ${MMseqs}/PF01699_Ca_exchangermerged_vsArchaea${array[$SLURM_ARRAY_TASK_ID]}_e03mmseq_fl_seq.fasta
+python getting_fasta_from_hit_extra_Arc.py ${MMseqs}/PF01699_Ca_exchangermerged_vsArchaea${array[$SLURM_ARRAY_TASK_ID]}_e03_mmseq.tsv MMSEQ ${Arc_TSV} ${MMseqs}/PF01699_Ca_exchangermerged_vsArchaea${array[$SLURM_ARRAY_TASK_ID]}_e03mmseq_fl_seq.fasta
 
 #--------------------------------------------------------MscL-------------------------------------------------------------------------
 module load MMseqs2/15-6f452-gompi-2023a
@@ -83,7 +83,7 @@ mmseqs easy-search -e 1.00E-03 -c 0.0 --threads 10 ${Seq}/PF01741_MscL_merged.fa
 
 module load Python/3.11.3-gimkl-2022a
 
-python getting_fasta_from_hit_extra_Euk.py ${MMseqs}/PF01741_MscLmerged_vsArchaea${array[$SLURM_ARRAY_TASK_ID]}_e03_mmseq.tsv MMSEQ ${Arc_TSV} ${MMseqs}/PF01741_MscLmerged_vsArchaea${array[$SLURM_ARRAY_TASK_ID]}_e03mmseq_fl_seq.fasta
+python getting_fasta_from_hit_extra_Arc.py ${MMseqs}/PF01741_MscLmerged_vsArchaea${array[$SLURM_ARRAY_TASK_ID]}_e03_mmseq.tsv MMSEQ ${Arc_TSV} ${MMseqs}/PF01741_MscLmerged_vsArchaea${array[$SLURM_ARRAY_TASK_ID]}_e03mmseq_fl_seq.fasta
 
 #--------------------------------------------------------MscL-------------------------------------------------------------------------
 module load MMseqs2/15-6f452-gompi-2023a
@@ -91,7 +91,7 @@ mmseqs easy-search -e 1.00E-03 -c 0.0 --threads 10 ${Seq}/PF01769_MgtE_merged.fa
 
 module load Python/3.11.3-gimkl-2022a
 
-python getting_fasta_from_hit_extra_Euk.py ${MMseqs}/PF01769_MgtEmerged_vsArchaea${array[$SLURM_ARRAY_TASK_ID]}_e03_mmseq.tsv MMSEQ ${Arc_TSV} ${MMseqs}/PF01769_MgtEmerged_vsArchaea${array[$SLURM_ARRAY_TASK_ID]}_e03mmseq_fl_seq.fasta
+python getting_fasta_from_hit_extra_Arc.py ${MMseqs}/PF01769_MgtEmerged_vsArchaea${array[$SLURM_ARRAY_TASK_ID]}_e03_mmseq.tsv MMSEQ ${Arc_TSV} ${MMseqs}/PF01769_MgtEmerged_vsArchaea${array[$SLURM_ARRAY_TASK_ID]}_e03mmseq_fl_seq.fasta
 
 #--------------------------------------------------------MnhE-------------------------------------------------------------------------
 module load MMseqs2/15-6f452-gompi-2023a
@@ -99,7 +99,7 @@ mmseqs easy-search -e 1.00E-03 -c 0.0 --threads 10 ${Seq}/PF01899_MnhE_merged.fa
 
 module load Python/3.11.3-gimkl-2022a
 
-python getting_fasta_from_hit_extra_Euk.py ${MMseqs}/PF01899_MnhEmerged_vsArchaea${array[$SLURM_ARRAY_TASK_ID]}_e03_mmseq.tsv MMSEQ ${Arc_TSV} ${MMseqs}/PF01899_MnhEmerged_vsArchaea${array[$SLURM_ARRAY_TASK_ID]}_e03mmseq_fl_seq.fasta
+python getting_fasta_from_hit_extra_Arc.py ${MMseqs}/PF01899_MnhEmerged_vsArchaea${array[$SLURM_ARRAY_TASK_ID]}_e03_mmseq.tsv MMSEQ ${Arc_TSV} ${MMseqs}/PF01899_MnhEmerged_vsArchaea${array[$SLURM_ARRAY_TASK_ID]}_e03mmseq_fl_seq.fasta
 
 #-------------------------------------------------------BCCT-------------------------------------------------------------------------
 module load MMseqs2/15-6f452-gompi-2023a
@@ -107,14 +107,14 @@ mmseqs easy-search -e 1.00E-03 -c 0.0 --threads 10 ${Seq}/PF02028_BCCT_merged.fa
 
 module load Python/3.11.3-gimkl-2022a
 
-python getting_fasta_from_hit_extra_Euk.py ${MMseqs}/PF02028_BCCTmerged_vsArchaea${array[$SLURM_ARRAY_TASK_ID]}_e03_mmseq.tsv MMSEQ ${Arc_TSV} ${MMseqs}/PF02028_BCCTmerged_vsArchaea${array[$SLURM_ARRAY_TASK_ID]}_e03mmseq_fl_seq.fasta
+python getting_fasta_from_hit_extra_Arc.py ${MMseqs}/PF02028_BCCTmerged_vsArchaea${array[$SLURM_ARRAY_TASK_ID]}_e03_mmseq.tsv MMSEQ ${Arc_TSV} ${MMseqs}/PF02028_BCCTmerged_vsArchaea${array[$SLURM_ARRAY_TASK_ID]}_e03mmseq_fl_seq.fasta
 #-------------------------------------------------------OtsB-------------------------------------------------------------------------
 module load MMseqs2/15-6f452-gompi-2023a
 mmseqs easy-search -e 1.00E-03 -c 0.0 --threads 10 ${Seq}/PF02358_OtsB_merged.fasta ${Arc_db} ${MMseqs}/PF02358_OtsBmerged_vsArchaea${array[$SLURM_ARRAY_TASK_ID]}_e03_mmseq.tsv Archaea${array[$SLURM_ARRAY_TASK_ID]}_tmp
 
 module load Python/3.11.3-gimkl-2022a
 
-python getting_fasta_from_hit_extra_Euk.py ${MMseqs}/PF02358_OtsBmerged_vsArchaea${array[$SLURM_ARRAY_TASK_ID]}_e03_mmseq.tsv MMSEQ ${Arc_TSV} ${MMseqs}/PF02358_OtsBmerged_vsArchaea${array[$SLURM_ARRAY_TASK_ID]}_e03mmseq_fl_seq.fasta
+python getting_fasta_from_hit_extra_Arc.py ${MMseqs}/PF02358_OtsBmerged_vsArchaea${array[$SLURM_ARRAY_TASK_ID]}_e03_mmseq.tsv MMSEQ ${Arc_TSV} ${MMseqs}/PF02358_OtsBmerged_vsArchaea${array[$SLURM_ARRAY_TASK_ID]}_e03mmseq_fl_seq.fasta
 
 #-------------------------------------------------------TrkH-------------------------------------------------------------------------
 module load MMseqs2/15-6f452-gompi-2023a
@@ -122,7 +122,7 @@ mmseqs easy-search -e 1.00E-03 -c 0.0 --threads 10 ${Seq}/PF02386_TrkH_merged.fa
 
 module load Python/3.11.3-gimkl-2022a
 
-python getting_fasta_from_hit_extra_Euk.py ${MMseqs}/PF02386_TrkHmerged_vsArchaea${array[$SLURM_ARRAY_TASK_ID]}_e03_mmseq.tsv MMSEQ ${Arc_TSV} ${MMseqs}/PF02386_TrkHmerged_vsArchaea${array[$SLURM_ARRAY_TASK_ID]}_e03mmseq_fl_seq.fasta
+python getting_fasta_from_hit_extra_Arc.py ${MMseqs}/PF02386_TrkHmerged_vsArchaea${array[$SLURM_ARRAY_TASK_ID]}_e03_mmseq.tsv MMSEQ ${Arc_TSV} ${MMseqs}/PF02386_TrkHmerged_vsArchaea${array[$SLURM_ARRAY_TASK_ID]}_e03mmseq_fl_seq.fasta
 
 #-------------------------------------------------------Kup-------------------------------------------------------------------------
 module load MMseqs2/15-6f452-gompi-2023a
@@ -130,7 +130,7 @@ mmseqs easy-search -e 1.00E-03 -c 0.0 --threads 10 ${Seq}/PF02705_Kup_merged.fas
 
 module load Python/3.11.3-gimkl-2022a
 
-python getting_fasta_from_hit_extra_Euk.py ${MMseqs}/PF02705_Kupmerged_vsArchaea${array[$SLURM_ARRAY_TASK_ID]}_e03_mmseq.tsv MMSEQ ${Arc_TSV} ${MMseqs}/PF02705_Kupmerged_vsArchaea${array[$SLURM_ARRAY_TASK_ID]}_e03mmseq_fl_seq.fasta
+python getting_fasta_from_hit_extra_Arc.py ${MMseqs}/PF02705_Kupmerged_vsArchaea${array[$SLURM_ARRAY_TASK_ID]}_e03_mmseq.tsv MMSEQ ${Arc_TSV} ${MMseqs}/PF02705_Kupmerged_vsArchaea${array[$SLURM_ARRAY_TASK_ID]}_e03mmseq_fl_seq.fasta
 
 #------------------------------------------------------Hppase-------------------------------------------------------------------------
 module load MMseqs2/15-6f452-gompi-2023a
@@ -138,7 +138,7 @@ mmseqs easy-search -e 1.00E-03 -c 0.0 --threads 10 ${Seq}/PF03030_HPPase_merged.
 
 module load Python/3.11.3-gimkl-2022a
 
-python getting_fasta_from_hit_extra_Euk.py ${MMseqs}/PF03030_HPPasemerged_vsArchaea${array[$SLURM_ARRAY_TASK_ID]}_e03_mmseq.tsv MMSEQ ${Arc_TSV} ${MMseqs}/PF03030_HPPasemerged_vsArchaea${array[$SLURM_ARRAY_TASK_ID]}_e03mmseq_fl_seq.fasta
+python getting_fasta_from_hit_extra_Arc.py ${MMseqs}/PF03030_HPPasemerged_vsArchaea${array[$SLURM_ARRAY_TASK_ID]}_e03_mmseq.tsv MMSEQ ${Arc_TSV} ${MMseqs}/PF03030_HPPasemerged_vsArchaea${array[$SLURM_ARRAY_TASK_ID]}_e03mmseq_fl_seq.fasta
 
 
 #------------------------------------------------------NhaC-------------------------------------------------------------------------
@@ -147,7 +147,7 @@ mmseqs easy-search -e 1.00E-03 -c 0.0 --threads 10 ${Seq}/PF03553_NhaC_merged.fa
 
 module load Python/3.11.3-gimkl-2022a
 
-python getting_fasta_from_hit_extra_Euk.py ${MMseqs}/PF03553_NhaCmerged_vsArchaea${array[$SLURM_ARRAY_TASK_ID]}_e03_mmseq.tsv MMSEQ ${Arc_TSV} ${MMseqs}/PF03553_NhaCmerged_vsArchaea${array[$SLURM_ARRAY_TASK_ID]}_e03mmseq_fl_seq.fasta
+python getting_fasta_from_hit_extra_Arc.py ${MMseqs}/PF03553_NhaCmerged_vsArchaea${array[$SLURM_ARRAY_TASK_ID]}_e03_mmseq.tsv MMSEQ ${Arc_TSV} ${MMseqs}/PF03553_NhaCmerged_vsArchaea${array[$SLURM_ARRAY_TASK_ID]}_e03mmseq_fl_seq.fasta
 
 #------------------------------------------------------NhaD-------------------------------------------------------------------------
 module load MMseqs2/15-6f452-gompi-2023a
@@ -155,7 +155,7 @@ mmseqs easy-search -e 1.00E-03 -c 0.0 --threads 10 ${Seq}/PF03600_NhaD_merged.fa
 
 module load Python/3.11.3-gimkl-2022a
 
-python getting_fasta_from_hit_extra_Euk.py ${MMseqs}/PF03600_NhaDmerged_vsArchaea${array[$SLURM_ARRAY_TASK_ID]}_e03_mmseq.tsv MMSEQ ${Arc_TSV} ${MMseqs}/PF03600_NhaDmerged_vsArchaea${array[$SLURM_ARRAY_TASK_ID]}_e03mmseq_fl_seq.fasta
+python getting_fasta_from_hit_extra_Arc.py ${MMseqs}/PF03600_NhaDmerged_vsArchaea${array[$SLURM_ARRAY_TASK_ID]}_e03_mmseq.tsv MMSEQ ${Arc_TSV} ${MMseqs}/PF03600_NhaDmerged_vsArchaea${array[$SLURM_ARRAY_TASK_ID]}_e03mmseq_fl_seq.fasta
 
 #------------------------------------------------------NhaB-------------------------------------------------------------------------
 module load MMseqs2/15-6f452-gompi-2023a
@@ -163,7 +163,7 @@ mmseqs easy-search -e 1.00E-03 -c 0.0 --threads 10 ${Seq}/PF06450_NhaB_merged.fa
 
 module load Python/3.11.3-gimkl-2022a
 
-python getting_fasta_from_hit_extra_Euk.py ${MMseqs}/PF06450_NhaBmerged_vsArchaea${array[$SLURM_ARRAY_TASK_ID]}_e03_mmseq.tsv MMSEQ ${Arc_TSV} ${MMseqs}/PF06450_NhaBmerged_vsArchaea${array[$SLURM_ARRAY_TASK_ID]}_e03mmseq_fl_seq.fasta
+python getting_fasta_from_hit_extra_Arc.py ${MMseqs}/PF06450_NhaBmerged_vsArchaea${array[$SLURM_ARRAY_TASK_ID]}_e03_mmseq.tsv MMSEQ ${Arc_TSV} ${MMseqs}/PF06450_NhaBmerged_vsArchaea${array[$SLURM_ARRAY_TASK_ID]}_e03mmseq_fl_seq.fasta
 
 #------------------------------------------------------KdP-------------------------------------------------------------------------
 module load MMseqs2/15-6f452-gompi-2023a
@@ -171,7 +171,7 @@ mmseqs easy-search -e 1.00E-03 -c 0.0 --threads 10 ${Seq}/PF03814_Kdp_merged.faa
 
 module load Python/3.11.3-gimkl-2022a
 
-python getting_fasta_from_hit_extra_Euk.py ${MMseqs}/PF03814_Kdpmerged_vsArchaea${array[$SLURM_ARRAY_TASK_ID]}_e03_mmseq.tsv MMSEQ ${Arc_TSV} ${MMseqs}/PF03814_Kdpmerged_vsArchaea${array[$SLURM_ARRAY_TASK_ID]}_e03mmseq_fl_seq.fasta
+python getting_fasta_from_hit_extra_Arc.py ${MMseqs}/PF03814_Kdpmerged_vsArchaea${array[$SLURM_ARRAY_TASK_ID]}_e03_mmseq.tsv MMSEQ ${Arc_TSV} ${MMseqs}/PF03814_Kdpmerged_vsArchaea${array[$SLURM_ARRAY_TASK_ID]}_e03mmseq_fl_seq.fasta
 
 #------------------------------------------------------OpuAC-------------------------------------------------------------------------
 module load MMseqs2/15-6f452-gompi-2023a
@@ -179,7 +179,7 @@ mmseqs easy-search -e 1.00E-03 -c 0.0 --threads 10 ${Seq}/PF04069_OpuAC_merged.f
 
 module load Python/3.11.3-gimkl-2022a
 
-python getting_fasta_from_hit_extra_Euk.py ${MMseqs}/PF04069_OpuACmerged_vsArchaea${array[$SLURM_ARRAY_TASK_ID]}_e03_mmseq.tsv MMSEQ ${Arc_TSV} ${MMseqs}/PF04069_OpuACmerged_vsArchaea${array[$SLURM_ARRAY_TASK_ID]}_e03mmseq_fl_seq.fasta
+python getting_fasta_from_hit_extra_Arc.py ${MMseqs}/PF04069_OpuACmerged_vsArchaea${array[$SLURM_ARRAY_TASK_ID]}_e03_mmseq.tsv MMSEQ ${Arc_TSV} ${MMseqs}/PF04069_OpuACmerged_vsArchaea${array[$SLURM_ARRAY_TASK_ID]}_e03mmseq_fl_seq.fasta
 
 #------------------------------------------------------TreT------------------------------------------------------------------------
 module load MMseqs2/15-6f452-gompi-2023a
@@ -187,7 +187,7 @@ mmseqs easy-search -e 1.00E-03 -c 0.0 --threads 10 ${Seq}/PF21269_TreT_merged.fa
 
 module load Python/3.11.3-gimkl-2022a
 
-python getting_fasta_from_hit_extra_Euk.py ${MMseqs}/PF21269_TreTmerged_vsArchaea${array[$SLURM_ARRAY_TASK_ID]}_e03_mmseq.tsv MMSEQ ${Arc_TSV} ${MMseqs}/PF21269_TreTmerged_vsArchaea${array[$SLURM_ARRAY_TASK_ID]}_e03mmseq_fl_seq.fasta
+python getting_fasta_from_hit_extra_Arc.py ${MMseqs}/PF21269_TreTmerged_vsArchaea${array[$SLURM_ARRAY_TASK_ID]}_e03_mmseq.tsv MMSEQ ${Arc_TSV} ${MMseqs}/PF21269_TreTmerged_vsArchaea${array[$SLURM_ARRAY_TASK_ID]}_e03mmseq_fl_seq.fasta
 
 #------------------------------------------------------NqrA------------------------------------------------------------------------
 module load MMseqs2/15-6f452-gompi-2023a
@@ -195,7 +195,7 @@ mmseqs easy-search -e 1.00E-03 -c 0.0 --threads 10 ${Seq}/PF24836_NqrA_merged.fa
 
 module load Python/3.11.3-gimkl-2022a
 
-python getting_fasta_from_hit_extra_Euk.py ${MMseqs}/PF24836_NqrAmerged_vsArchaea${array[$SLURM_ARRAY_TASK_ID]}_e03_mmseq.tsv MMSEQ ${Arc_TSV} ${MMseqs}/PF24836_NqrAmerged_vsArchaea${array[$SLURM_ARRAY_TASK_ID]}_e03mmseq_fl_seq.fasta
+python getting_fasta_from_hit_extra_Arc.py ${MMseqs}/PF24836_NqrAmerged_vsArchaea${array[$SLURM_ARRAY_TASK_ID]}_e03_mmseq.tsv MMSEQ ${Arc_TSV} ${MMseqs}/PF24836_NqrAmerged_vsArchaea${array[$SLURM_ARRAY_TASK_ID]}_e03mmseq_fl_seq.fasta
 
 #------------------------------------------------------KimA------------------------------------------------------------------------
 module load MMseqs2/15-6f452-gompi-2023a
@@ -203,7 +203,7 @@ mmseqs easy-search -e 1.00E-03 -c 0.0 --threads 10 ${Seq}/PTHR47704_KimA.fasta $
 
 module load Python/3.11.3-gimkl-2022a
 
-python getting_fasta_from_hit_extra_Euk.py ${MMseqs}/PF24836_NqrAmerged_vsArchaea${array[$SLURM_ARRAY_TASK_ID]}_e03_mmseq.tsv MMSEQ ${Arc_TSV} ${MMseqs}/PTHR47704_KimAmerged_vsArchaea${array[$SLURM_ARRAY_TASK_ID]}_e03mmseq_fl_seq.fasta
+python getting_fasta_from_hit_extra_Arc.py ${MMseqs}/PF24836_NqrAmerged_vsArchaea${array[$SLURM_ARRAY_TASK_ID]}_e03_mmseq.tsv MMSEQ ${Arc_TSV} ${MMseqs}/PTHR47704_KimAmerged_vsArchaea${array[$SLURM_ARRAY_TASK_ID]}_e03mmseq_fl_seq.fasta
 
 #------------------------------------------------------Betain_dehydrogenase------------------------------------------------------------------------
 module load MMseqs2/15-6f452-gompi-2023a
@@ -211,4 +211,4 @@ mmseqs easy-search -e 1.00E-03 -c 0.0 --threads 10 ${Seq}/TIGR01804.1_Betain-ald
 
 module load Python/3.11.3-gimkl-2022a
 
-python getting_fasta_from_hit_extra_Euk.py ${MMseqs}/TIGR01804.1_Betain-aldehyde_dehydrogenasemerged_vsArchaea${array[$SLURM_ARRAY_TASK_ID]}_e03_mmseq.tsv MMSEQ ${Arc_TSV} ${MMseqs}/TIGR01804.1_Betain-aldehyde_dehydrogenasemerged_vsArchaea${array[$SLURM_ARRAY_TASK_ID]}_e03mmseq_fl_seq.fasta
+python getting_fasta_from_hit_extra_Arc.py ${MMseqs}/TIGR01804.1_Betain-aldehyde_dehydrogenasemerged_vsArchaea${array[$SLURM_ARRAY_TASK_ID]}_e03_mmseq.tsv MMSEQ ${Arc_TSV} ${MMseqs}/TIGR01804.1_Betain-aldehyde_dehydrogenasemerged_vsArchaea${array[$SLURM_ARRAY_TASK_ID]}_e03mmseq_fl_seq.fasta
