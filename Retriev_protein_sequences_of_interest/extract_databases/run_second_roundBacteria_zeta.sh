dfd9d6cb188126f4958accc0a409aa65e4296c49
MAdriaansens <132375271+MAdriaansens@users.noreply.github.com>
2026-02-25 09:42:53 +1300

    Create run_second_roundBacteria_zeta.sh

---
+++ Retriev_protein_sequences_of_interest/extract_databases/run_second_roundBacteria_zeta.sh
@@ -0,0 +1,97 @@
+#!/bin/bash -e
+#SBATCH --account       uc04105
+#SBATCH --job-name      Bac_Zeta_round2
+#SBATCH --time          102:00:00
+#SBATCH --mem           100GB
+#SBATCH --cpus-per-task 25
+#SBATCH --error         slurm_outputB2/Bac2Zeta_%A-%a.err
+#SBATCH --output        slurm_outputB2/Bac2Zeta_%A-%a.out
+#SBATCH --array         0-12
+declare -a array=($(seq 0 12))
+Bac_TSV=/nesi/nobackup/uc04105/new_databases_May/GTDB_226/Bacteria_GTDB226_protein_May92025.tsv
+Bac_db=/nesi/nobackup/uc04105/new_databases_May/GTDB_226/ARCDB/fasta/Bacteria_GTDB226_protein_May92025_subset${array[$SLURM_ARRAY_TASK_ID]}.fasta
+MMseqs=/nesi/nobackup/uc04105/cross_biome_metagenome/Protein/results/second_round/mmseqs/Bacteria
+Seq=/nesi/nobackup/uc04105/cross_biome_metagenome/Protein/results/initial_round/merged_all_three_domains
+
+
+
+#----------------------------------------------------------RpL4-------------------------------------------------------------------------
+module load MMseqs2/15-6f452-gompi-2023a
+mmseqs easy-search -e 1.00E-03 -c 0.0 --threads 25 ${Seq}/PF00573_RpL4_merged.fasta ${Bac_db} ${MMseqs}/PF00573_RpL4merged_vsBacteria${array[$SLURM_ARRAY_TASK_ID]}_e03_mmseq.tsv Bacteria${array[$SLURM_ARRAY_TASK_ID]}_tmp
+
+module load Python/3.11.3-gimkl-2022a
+
+python getting_fasta_from_hit_extra_Arc.py ${MMseqs}/PF00573_RpL4merged_vsBacteria${array[$SLURM_ARRAY_TASK_ID]}_e03_mmseq.tsv MMSEQ ${Bac_TSV} ${MMseqs}/PF00573_RpL4merged_vsBacteria${array[$SLURM_ARRAY_TASK_ID]}_e03mmseq_fl_seq.fasta
+
+
+#--------------------------------------------------------MgtE-------------------------------------------------------------------------
+module load MMseqs2/15-6f452-gompi-2023a
+mmseqs easy-search -e 1.00E-03 -c 0.0 --threads 25 ${Seq}/PF01769_MgtE_merged.faa ${Bac_db} ${MMseqs}/PF01769_MgtEmerged_vsBacteria${array[$SLURM_ARRAY_TASK_ID]}_e03_mmseq.tsv Bacteria${array[$SLURM_ARRAY_TASK_ID]}_tmp
+
+module load Python/3.11.3-gimkl-2022a
+
+python getting_fasta_from_hit_extra_Arc.py ${MMseqs}/PF01769_MgtEmerged_vsBacteria${array[$SLURM_ARRAY_TASK_ID]}_e03_mmseq.tsv MMSEQ ${Bac_TSV} ${MMseqs}/PF01769_MgtEmerged_vsBacteria${array[$SLURM_ARRAY_TASK_ID]}_e03mmseq_fl_seq.fasta
+
+#--------------------------------------------------------MnhE-------------------------------------------------------------------------
+module load MMseqs2/15-6f452-gompi-2023a
+mmseqs easy-search -e 1.00E-03 -c 0.0 --threads 25 ${Seq}/PF01899_MnhE_merged.fasta ${Bac_db} ${MMseqs}/PF01899_MnhEmerged_vsBacteria${array[$SLURM_ARRAY_TASK_ID]}_e03_mmseq.tsv Bacteria${array[$SLURM_ARRAY_TASK_ID]}_tmp
+
+module load Python/3.11.3-gimkl-2022a
+
+python getting_fasta_from_hit_extra_Arc.py ${MMseqs}/PF01899_MnhEmerged_vsBacteria${array[$SLURM_ARRAY_TASK_ID]}_e03_mmseq.tsv MMSEQ ${Bac_TSV} ${MMseqs}/PF01899_MnhEmerged_vsBacteria${array[$SLURM_ARRAY_TASK_ID]}_e03mmseq_fl_seq.fasta
+
+#-------------------------------------------------------BCCT-------------------------------------------------------------------------
+module load MMseqs2/15-6f452-gompi-2023a
+mmseqs easy-search -e 1.00E-03 -c 0.0 --threads 25 ${Seq}/PF02028_BCCT_merged.fasta ${Bac_db} ${MMseqs}/PF02028_BCCTmerged_vsBacteria${array[$SLURM_ARRAY_TASK_ID]}_e03_mmseq.tsv Bacteria${array[$SLURM_ARRAY_TASK_ID]}_tmp
+
+module load Python/3.11.3-gimkl-2022a
+
+python getting_fasta_from_hit_extra_Arc.py ${MMseqs}/PF02028_BCCTmerged_vsBacteria${array[$SLURM_ARRAY_TASK_ID]}_e03_mmseq.tsv MMSEQ ${Bac_TSV} ${MMseqs}/PF02028_BCCTmerged_vsBacteria${array[$SLURM_ARRAY_TASK_ID]}_e03mmseq_fl_seq.fasta
+#-------------------------------------------------------OtsB-------------------------------------------------------------------------
+module load MMseqs2/15-6f452-gompi-2023a
+mmseqs easy-search -e 1.00E-03 -c 0.0 --threads 25 ${Seq}/PF02358_OtsB_merged.fasta ${Bac_db} ${MMseqs}/PF02358_OtsBmerged_vsBacteria${array[$SLURM_ARRAY_TASK_ID]}_e03_mmseq.tsv Bacteria${array[$SLURM_ARRAY_TASK_ID]}_tmp
+
+module load Python/3.11.3-gimkl-2022a
+
+python getting_fasta_from_hit_extra_Arc.py ${MMseqs}/PF02358_OtsBmerged_vsBacteria${array[$SLURM_ARRAY_TASK_ID]}_e03_mmseq.tsv MMSEQ ${Bac_TSV} ${MMseqs}/PF02358_OtsBmerged_vsBacteria${array[$SLURM_ARRAY_TASK_ID]}_e03mmseq_fl_seq.fasta
+
+#-------------------------------------------------------TrkH-------------------------------------------------------------------------
+module load MMseqs2/15-6f452-gompi-2023a
+mmseqs easy-search -e 1.00E-03 -c 0.0 --threads 25 ${Seq}/PF02386_TrkH_merged.faa ${Bac_db} ${MMseqs}/PF02386_TrkHmerged_vsBacteria${array[$SLURM_ARRAY_TASK_ID]}_e03_mmseq.tsv Bacteria${array[$SLURM_ARRAY_TASK_ID]}_tmp
+
+module load Python/3.11.3-gimkl-2022a
+
+python getting_fasta_from_hit_extra_Arc.py ${MMseqs}/PF02386_TrkHmerged_vsBacteria${array[$SLURM_ARRAY_TASK_ID]}_e03_mmseq.tsv MMSEQ ${Bac_TSV} ${MMseqs}/PF02386_TrkHmerged_vsBacteria${array[$SLURM_ARRAY_TASK_ID]}_e03mmseq_fl_seq.fasta
+
+#-------------------------------------------------------Kup-------------------------------------------------------------------------
+module load MMseqs2/15-6f452-gompi-2023a
+mmseqs easy-search -e 1.00E-03 -c 0.0 --threads 25 ${Seq}/PF02705_Kup_merged.fasta ${Bac_db} ${MMseqs}/PF02705_Kupmerged_vsBacteria${array[$SLURM_ARRAY_TASK_ID]}_e03_mmseq.tsv Bacteria${array[$SLURM_ARRAY_TASK_ID]}_tmp
+
+module load Python/3.11.3-gimkl-2022a
+
+python getting_fasta_from_hit_extra_Arc.py ${MMseqs}/PF02705_Kupmerged_vsBacteria${array[$SLURM_ARRAY_TASK_ID]}_e03_mmseq.tsv MMSEQ ${Bac_TSV} ${MMseqs}/PF02705_Kupmerged_vsBacteria${array[$SLURM_ARRAY_TASK_ID]}_e03mmseq_fl_seq.fasta
+
+#------------------------------------------------------Hppase-------------------------------------------------------------------------
+module load MMseqs2/15-6f452-gompi-2023a
+mmseqs easy-search -e 1.00E-03 -c 0.0 --threads 25 ${Seq}/PF03030_HPPase_merged.faa ${Bac_db} ${MMseqs}/PF03030_HPPasemerged_vsBacteria${array[$SLURM_ARRAY_TASK_ID]}_e03_mmseq.tsv Bacteria${array[$SLURM_ARRAY_TASK_ID]}_tmp
+
+module load Python/3.11.3-gimkl-2022a
+
+python getting_fasta_from_hit_extra_Arc.py ${MMseqs}/PF03030_HPPasemerged_vsBacteria${array[$SLURM_ARRAY_TASK_ID]}_e03_mmseq.tsv MMSEQ ${Bac_TSV} ${MMseqs}/PF03030_HPPasemerged_vsBacteria${array[$SLURM_ARRAY_TASK_ID]}_e03mmseq_fl_seq.fasta
+
+
+#------------------------------------------------------NhaC-------------------------------------------------------------------------
+module load MMseqs2/15-6f452-gompi-2023a
+mmseqs easy-search -e 1.00E-03 -c 0.0 --threads 25 ${Seq}/PF03553_NhaC_merged.faa ${Bac_db} ${MMseqs}/PF03553_NhaCmerged_vsBacteria${array[$SLURM_ARRAY_TASK_ID]}_e03_mmseq.tsv Bacteria${array[$SLURM_ARRAY_TASK_ID]}_tmp
+
+module load Python/3.11.3-gimkl-2022a
+
+python getting_fasta_from_hit_extra_Arc.py ${MMseqs}/PF03553_NhaCmerged_vsBacteria${array[$SLURM_ARRAY_TASK_ID]}_e03_mmseq.tsv MMSEQ ${Bac_TSV} ${MMseqs}/PF03553_NhaCmerged_vsBacteria${array[$SLURM_ARRAY_TASK_ID]}_e03mmseq_fl_seq.fasta
+
+#------------------------------------------------------NhaD-------------------------------------------------------------------------
+module load MMseqs2/15-6f452-gompi-2023a
+mmseqs easy-search -e 1.00E-03 -c 0.0 --threads 25 ${Seq}/PF03600_NhaD_merged.faa ${Bac_db} ${MMseqs}/PF03600_NhaDmerged_vsBacteria${array[$SLURM_ARRAY_TASK_ID]}_e03_mmseq.tsv Bacteria${array[$SLURM_ARRAY_TASK_ID]}_tmp
+
+module load Python/3.11.3-gimkl-2022a
+
+python getting_fasta_from_hit_extra_Arc.py ${MMseqs}/PF03600_NhaDmerged_vsBacteria${array[$SLURM_ARRAY_TASK_ID]}_e03_mmseq.tsv MMSEQ ${Bac_TSV} ${MMseqs}/PF03600_NhaDmerged_vsBacteria${array[$SLURM_ARRAY_TASK_ID]}_e03mmseq_fl_seq.fasta
